81476516843f2c109815abdefde45ec6869ca68e
beepboop-lettuce <ericaharris0486@gmail.com>
2022-02-11 14:44:23 -0600

    if statement for err defined var max if statement for len(os.Args)

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func main() {
+	max, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("%v is not a number\n", max)
+	}
+
+	if len(os.Args) < 2 {
+		fmt.Println("Give me the size of the table.")
+	}
 
 }
