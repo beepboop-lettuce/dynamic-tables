3167e8eda7924935ee238b882b88f55bfb3e95cd
beepboop-lettuce <ericaharris0486@gmail.com>
2022-02-11 14:48:20 -0600

    loop for horizontal headers

--- main.go
+++ main.go
@@ -9,11 +9,16 @@ import (
 func main() {
 	max, err := strconv.Atoi(os.Args[1])
 	if err != nil {
-		fmt.Printf("%v is not a number\n", max)
+		fmt.Printf("%q is not a number\n", max)
 	}
 
 	if len(os.Args) < 2 {
 		fmt.Println("Give me the size of the table.")
+	}
+
+	fmt.Printf("%5s", "X")
+	for i := 0; i <= max; i++ {
+		fmt.Printf("%5d", i)
 	}
 
 }
