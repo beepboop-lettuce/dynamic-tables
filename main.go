e92db5f579b5f72caf2b9962d6d62563b6913d84
beepboop-lettuce <ericaharris0486@gmail.com>
2022-02-14 13:07:22 -0600

    fixed loop for vertical and horizontal headers fixed inner loop for populating cells added println  in between rows

--- main.go
+++ main.go
@@ -7,18 +7,35 @@ import (
 )
 
 func main() {
-	max, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Printf("%q is not a number\n", max)
+
+	args := os.Args
+
+	if len(args) != 2 {
+		fmt.Println("Give me the size of the table.")
+		return
 	}
 
-	if len(os.Args) < 2 {
-		fmt.Println("Give me the size of the table.")
+	max, err := strconv.Atoi(os.Args[1])
+	if err != nil || max <= 0 {
+		fmt.Println("Wrong size.")
+		return
 	}
 
 	fmt.Printf("%5s", "X")
 	for i := 0; i <= max; i++ {
-		fmt.Printf("%5d\n", i)
+		fmt.Printf("%5d", i)
+	}
+
+	fmt.Println()
+
+	for i := 0; i <= max; i++ {
+		fmt.Printf("%5d", i)
+
+		for j := 0; j <= max; j++ {
+			fmt.Printf("%5d", i*j)
+		}
+
+		fmt.Println()
 	}
 
 }
